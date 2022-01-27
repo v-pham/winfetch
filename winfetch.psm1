a9917b2c27c1eed425095e3e54a8926d4adc9c62
vvpham <vvpham@null.foobar.tld>
2022-01-26 21:43:17 -0500

    Fixed nano memory calculation

--- winfetch.psm1
+++ winfetch.psm1
@@ -220,7 +220,7 @@ function Write-SystemInformation {
           "gpu" { $SystemProperty["GPU"] = [string]$(((Get-PnpDevice -Class Display -Status OK | Where-Object { $_.FriendlyName -notlike 'Microsoft*Remote*' }).FriendlyName -replace '\(R\)')  -join ', ') }
           "memory" {
             if($IsNano){
-              $Memory = Get-CimInstance Win32_PhysicalMemory | Select-Object -ExpandProperty Capacity
+              $Memory = Get-CimInstance -Class Win32_PerfRawData_Counters_HyperVDynamicMemoryIntegrationService | Select-Object -ExpandProperty MaximumMemoryMBytes
             }else {
               $Memory = wmic MemoryChip get Capacity | Where-Object { $_.Length -gt 0 -and $_.Trim() -notlike 'Capacity' }
             }
