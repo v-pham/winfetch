9a65f8d3262f395f6aa34f902903a840142e6b0b
vvpham <vvpham@null.foobar.tld>
2022-01-28 17:24:43 -0500

    Removed tools dir

---
+++ Get-OSReleaseInfo.ps1
@@ -0,0 +1,40 @@
+function Get-OSReleaseInfo {
+  [Alias('osinfo')]
+  param(
+    [Parameter(Position=0)][string[]]$Key,
+    [switch]$RegistryOnly,
+    [switch]$FileOnly,
+    [Alias('Path')][string]$FilePath="$Env:SystemRoot\system32\drivers\etc\os-release"
+  )
+
+  $OSReleaseInfo = [ordered]@{}
+  
+  if(!$FileOnly.IsPresent){
+    $Keys = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" 
+    $Keys | Get-Member | Where-Object { 
+      $_.Name -notlike "PS*" -and $_.MemberType -eq "NoteProperty" -and $Keys."$_" -ne ""
+    } | foreach {
+      $OSReleaseInfo."$($_.Name)" = $Keys.$($_.Name) 
+    }
+  }
+
+  if(!$RegistryOnly.IsPresent){
+    if((Test-Path $FilePath) -and ($null -ne $(Get-Content $FilePath))){
+      $OSReleaseFile = Get-Content $FilePath
+      $OSReleaseFile.split([System.Environment]::NewLine) | foreach {
+        $Value_raw = $_ -replace "$($_.Split('=')[0])="
+        $OSReleaseInfo."$($_.Split('=')[0])" = $Value_raw.Substring(1,$Value_raw.Length-2)
+      }  
+    }elseif($FileOnly.IsPresent){
+      Write-Error "The os-release file does not exist." -ErrorAction Continue
+    }
+  }
+
+  if($Key.Count -gt 0){
+    $OSReleaseInfo.GetEnumerator() | Where-Object { $Key.ToLower().Contains($_.Name.ToLower()) }
+  }else{
+    return $OSReleaseInfo
+  }
+}
+
+Export-ModuleMember -Function Get-OSReleaseInfo
