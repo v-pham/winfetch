2d966b41ec990ababcfceac6e6418e04994c4698
vvpham <victorvpham@gmail.com>
2022-05-26 21:19:05 -0400

    Updated

--- Write-OSRelease.ps1
+++ Write-OSRelease.ps1
@@ -59,40 +59,4 @@ function Write-OSRelease {
   }
 }
 
-Export-ModuleMember -Function 'Write-OSRelease','Test-Elevated' -Alias @('Export-OSRelease', 'IsAdmin')
-
-      }
-    }
-    $SystemInfo_Map = @{}
-  }
-
-  process {
-    $(systeminfo | Out-String).split([Environment]::NewLine) | Where-Object { $null -ne $_ } | foreach {
-      $SystemInfo_Map["$($_.split(':')[0] -replace ' ')"] = $_.split(':')[1]
-    }
-    @('HostName','OSName','Domain','SystemManufacturer','SystemModel','BIOSVersion') | ForEach-Object {
-      switch($_){
-        'OSName' { $Key = 'ProductName'; $Value = $SystemInfo_Map["$_"] -replace 'Microsoft ' }
-        'Domain' { $Key = 'MachineDomain'; $Value = $SystemInfo_Map["$_"] }
-        default { $Key = $_; $Value = $SystemInfo_Map["$_"] }
-      }
-      $OSRelease.Map[$Key] = [string]$('"' + $Value.Trim() + '"')
-    }
-  }
-
-  end {
-    if(Test-Elevated){
-      New-Item -Path $OSRelease.FilePath -Value $null -Force | Out-Null
-      $OSRelease.Map.GetEnumerator() | ForEach-Object {
-        "$($_.Key)=$($_.Value)" | Out-File $OSRelease.FilePath -Append
-      }
-      $Output = $(Get-Item $OSRelease.FilePath)
-    }else{
-      Write-Error "No permissions to write to the /etc/os-release file. Key-values returned as output." -ErrorAction Continue
-      $Output = $OSRelease.Map
-    }
-    return $Output
-  }
-}
-
 Export-ModuleMember -Function 'Write-OSRelease','Test-Elevated' -Alias @('Export-OSRelease', 'IsAdmin') -ErrorAction SilentlyContinue
